77e85c2e8e420d546afcf4279746194d1a007c51
leiflabianca <leif.labianca@gmail.com>
2024-10-09 15:55:32 -0400

    implemented comments and completed reflection

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
+# Programmers:  Leif LaBianca
+# Course:  CS151, Professor Zee
+# Due Date: October 9, 23:59
+# Programming Assignment:  PA1
+# Problem Statement:  This code has the user play a game about designing a piece of clothing to sell. Given a set of questions and user inputs, the code will determine the success of the product and what it ultimately sells for
+# Data In: Type of clothing, count of specific detail on clothing, thickness of the clothing item, a "yes" or "no" input
+# Data Out:  Price the clothing sold for and how much more their clothing item could have sold for
+# Credits: All original code
+
+# presets values to placeholder values
 high_street_win = "blank"
+chosen_clothing = "blank"
+# sets variables to "no" so by default the while loops will
+# continue looping unless another value is set
 pass_thru1 = "no"
 pass_thru2 = "no"
-chosen_clothing = "blank"
+# explains game to user
 print("Welcome to the fashion adventure game.\nYou will design either a jacket, pair of pants, or scarf.\nThe goal is to create a piece of clothing you think designers and customers will like.\nYou will get to see what your item sold for at the end.\n")
+# requests the user select one of the three clothing options continuously until a valid input is detected
 while pass_thru1 == "no":
     chosen_clothing = str.lower(input("Would you like to design a jacket, pants, or scarf? "))
     if chosen_clothing == "pants":
@@ -13,7 +27,9 @@ while pass_thru1 == "no":
         pass_thru1 = "yes"
     else:
         print("This value is not accepted. Please input 'jacket' 'pants' or 'scarf'\n")
-
+# based on the input clothing item, the user is prompted with a variety of questions
+# that will determine the outcome of the product. If the user input meets both criteria,
+# it was a smash hit. If it met just one of the criteria, it did well. If it met neither, it did poorly
 if chosen_clothing == "jacket":
     jacket_buttons = int(input("How many buttons will your jacket have? "))
     jacket_thickness = float(input("How thick will your jacket be in inches? "))
@@ -50,7 +66,9 @@ elif chosen_clothing == "scarf":
     else:
         print('Your scarf did not do well and needs major adjustments.\n')
         win_status = 'Loss'
-
+# if the user met both criteria, the code prompts them on whether they wish to enter a high street
+# fashion competition. If they enter, they win only if they meet both of the below criteria. Note that
+# entrance is not mandatory to continue the game
 if win_status == "Full win":
     while pass_thru2 == "no":
         high_street_query = str.lower(input(f"Your {chosen_clothing} qualified for a high street fashion contest!\nWould you like to enter your {chosen_clothing} to it? "))
@@ -82,18 +100,21 @@ if win_status == "Full win":
             print("Understood.\n")
         else:
             print("You did not enter a valid option. Please enter 'yes' or 'no'.\n" )
-
+# determines the shelf price of the users design clothing item given the amount of criteria met. Note
+# that if the user met both criteria and participated in the high street fashion competition, the
+# price will raise with a win and lower with a loss as such
 if win_status == "Full win":
     sell_price = 200
     if high_street_win == "Win":
-        sell_price = 200 + 100
+        sell_price = sell_price + 100
     elif high_street_win == "Loss":
-        sell_price = 200 - 50
+        sell_price = sell_price - 50
 elif win_status == "Half win":
     sell_price = 100
 elif win_status == "Loss":
     sell_price = 25
-
+# prints the end shelf price and the distance they were from the maximum price, encouraging them to
+# play again regardless of outcome
 profit_potential = 300 - sell_price
 print(f'Your {chosen_clothing} is now on the shelves for ${sell_price}!')
 if sell_price < 300:
